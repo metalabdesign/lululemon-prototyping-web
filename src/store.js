55396ef3b5942c1bca1cde9d823f57f9ccd197b8
Chris Gat <chris.gat@metalabdesign.com>
2019-02-15 08:46:35 -0800

    Add some top level actions for ambassador details. Needs refactor later

--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as AmbassadorsApi from "./api/ambassador_api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    ambassadorDetails: {}
+  },
+  mutations: {
+    fetchDetailsSucceeded (state, payload) {
+      state.ambassadorDetails = payload
+    }
+  },
+  actions: {
+    async fetchDetails ({ commit }, id) {
+      const payload = await AmbassadorsApi.fetch(id);
+      commit('fetchDetailsSucceeded', payload)
+    }
+  }
 });
