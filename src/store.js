79239756b0ea3340cfb6ac43d8bb0d751a676882
Chris Gat <chris.gat@metalabdesign.com>
2019-02-20 14:16:04 -0800

    Modularize the ambassador store with namespace

--- src/store.js
+++ src/store.js
@@ -5,18 +5,35 @@ import * as AmbassadorsApi from "./api/ambassador_api";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    ambassadorDetails: {}
-  },
-  mutations: {
-    fetchDetailsSucceeded (state, payload) {
-      state.ambassadorDetails = payload
-    }
-  },
-  actions: {
-    async fetchDetails ({ commit }, id) {
-      const payload = await AmbassadorsApi.fetch(id);
-      commit('fetchDetailsSucceeded', payload)
+  modules: {
+    ambassador: {
+      namespaced: true,
+      state: {
+        details: null,
+        loading: false,
+      },
+      mutations: {
+        fetchDetailsStarted (state) {
+          state.loading = true;
+        },
+        fetchDetailsCompleted (state) {
+          state.loading = false;
+        },
+        fetchDetailsFailed (state, payload) {
+          state.error = payload;
+        },
+        fetchDetailsSucceeded (state, payload) {
+          state.details = payload;
+        },
+      },
+      actions: {
+        async fetchDetails ({ commit }, id) {
+          commit('fetchDetailsStarted');
+          const payload = await AmbassadorsApi.fetch(id);
+          commit('fetchDetailsSucceeded', payload)
+          commit('fetchDetailsCompleted');
+        }
+      },
     }
   }
 });
