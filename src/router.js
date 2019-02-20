d79f5176ff97742a9f51c6fa74ca6d98383f0edb
Chris Gat <chris.gat@metalabdesign.com>
2019-02-20 14:20:58 -0800

    Clean up

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 import Ambassador from "./views/Ambassador";
 
 Vue.use(Router);
@@ -12,15 +11,6 @@ export default new Router({
     {
       path: "/",
       redirect: "/ambassadors",
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
       path: "/ambassadors",
